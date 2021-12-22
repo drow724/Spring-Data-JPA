2398043170cbf75b3203e64e700fc0099b2929c4
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-12-22 21:28:06 +0900

    순수 JPA 기반 리포지토리 만들기

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -2,6 +2,8 @@ package study.datajpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +16,6 @@ import study.datajpa.entity.Member;
 @Transactional
 @Rollback(false)
 public class MemberJpaRepositoryTest {
-	
 	@Autowired
 	MemberJpaRepository memberJpaRepository;
 
@@ -27,5 +28,32 @@ public class MemberJpaRepositoryTest {
 		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 		assertThat(findMember).isEqualTo(member); // JPA 엔티티 동일성 보장
 	}
-	
+
+	@Test
+	public void basicCRUD() {
+		Member member1 = new Member("member1");
+		Member member2 = new Member("member2");
+		memberJpaRepository.save(member1);
+		memberJpaRepository.save(member2);
+		// 단건 조회 검증
+		Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+		Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+		assertThat(findMember1).isEqualTo(member1);
+		assertThat(findMember2).isEqualTo(member2);
+		// 리스트 조회 검증
+		List<Member> all = memberJpaRepository.findAll();
+		assertThat(all.size()).isEqualTo(2);
+		// 카운트 검증
+		long count = memberJpaRepository.count();
+		assertThat(count).isEqualTo(2);
+		
+		//변경 감지 (굳이 update문을 쓸 필요 없다)
+		//findMember1.setUsername("member!!!");
+				
+		// 삭제 검증
+		memberJpaRepository.delete(member1);
+		memberJpaRepository.delete(member2);
+		long deletedCount = memberJpaRepository.count();
+		assertThat(deletedCount).isEqualTo(0);
+	}
 }
