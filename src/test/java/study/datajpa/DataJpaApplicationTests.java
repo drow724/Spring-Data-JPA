469469147fd119e9c841a893154b6497e48c007e
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-12-22 21:37:29 +0900

    공통 인터페이스 설정

--- src/test/java/study/datajpa/DataJpaApplicationTests.java
+++ src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -2,8 +2,10 @@ package study.datajpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest
+//@EnableJpaRepositories(basePackages = "study.datajpa.repository") 스프링 부트는 필요X
 class DataJpaApplicationTests {
 
 	@Test
